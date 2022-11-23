53846b03a92d1267bf741f6d4c7ba02c87d25b9a
Bian Lee <bianlee2004@gmail.com>
2022-11-23 14:30:22 -0800

    Web.givenProvider

--- client/src/buyBian.js
+++ client/src/buyBian.js
@@ -1,5 +1,5 @@
 const Web3 = require('web3')
-const web3 = new Web3(window.web3.currentProvider); 
+const web3 = new Web3(Web3.givenProvider); 
 // const web3 = new Web3(new Web3.providers.HttpProvider('https://goerli.infura.io/v3/7f36e75226334b0498ce7547dca2bc14'));
 
 //const tokenContract = "0x861A87be2F0b630e1da20b49065eFa4554f4514b" 
--- client/src/getBianTokenBalance.js
+++ client/src/getBianTokenBalance.js
@@ -1,5 +1,5 @@
 const Web3 = require('web3');
-const web3 = new Web3(window.web3.currentProvider); 
+const web3 = new Web3(Web3.givenProvider); 
 
 // const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/7f36e75226334b0498ce7547dca2bc14'));
 
